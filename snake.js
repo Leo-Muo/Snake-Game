7fb9445357e927fcf32255ab7cd5224879457115
Virgil Muoghara <v.muoghara@gmail.com>
2020-08-21 11:40:15 +0300

    Added eating growth function

--- snake.js
+++ snake.js
@@ -1,7 +1,7 @@
 class Snake {
   constructor() {
     this.body = [];
-    this.body[0] = createVector(0, 0);
+    this.head = createVector(floor(w / 2), floor(h / 2));
     this.xdir = 0;
     this.ydir = 0;
   }
@@ -12,18 +12,22 @@ class Snake {
   }
 
   update() {
-    this.body[0].x += this.xdir;
-    this.body[0].y += this.ydir;
+    this.body.shift();
+    this.head.x += this.xdir;
+    this.head.y += this.ydir;
+    this.body.push(this.head.copy());
   }
 
-  //
+  grow() {
+    this.body.push(this.head.copy());
+  }
 
   eat(pos) {
-    let x = this.body[0].x;
-    let y = this.body[0].y;
+    let x = this.head.x;
+    let y = this.head.y;
 
     if (x == pos.x && y == pos.y) {
-      //this.grow();
+      this.grow();
       return true;
     }
 
@@ -32,7 +36,13 @@ class Snake {
 
   show() {
     noStroke();
-    fill(0);
-    rect(this.body[0].x, this.body[0].y, 1, 1);
+
+    for (let i = 0; i < this.body.length; i++) {
+      fill(0, 26, 0);
+      rect(this.body[i].x, this.body[i].y, 1, 1);
+    }
+
+    fill(51, 102, 0);
+    rect(this.head.x, this.head.y, 1, 1);
   }
 }
